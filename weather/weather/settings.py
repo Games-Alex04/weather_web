c7f445a8bc0d4a4d83ece328e2d676d4ff6b5634
Games-Alex04 <alex205687@gmail.com>
2025-05-22 17:46:26 +0530

    vercel commit

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8d=f-vk(7o9p#eu$6j#3$9=-i((8!pl5&3&gjbx+chn&yj3qoy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.222.152','127.0.0.1',]
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'panachandbhaifarsan.in']
 
 
 # Application definition
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'weather.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
